89292b39dc9d39f2bc5731fb07c305017233cb59
michal szczepanski <mike@michals-MacBook-Air.local>
2020-12-26 19:44:43 +0100

    website html and scss finished - not working properly on safari

---
+++ php/email-form.php
@@ -0,0 +1,15 @@
+<?php
+
+//   $name = $_POST['fname'];
+//   $visitor_email = $_POST['email'];
+//   $message = $_POST['message'];
+
+//   $to = "michal.m.szczepanski@gmail.com";
+
+//   $headers = "From: $email_from \r\n";
+
+//   $headers .= "Reply-To: $visitor_email \r\n";
+
+//   mail($to,$email_subject,$email_body,$headers);
+
+ ?>
